a189409b3cff2b46d410d260a783b609abe7e2aa
Paweł Wójsik <pablo8899@interia.pl>
2020-04-05 09:30:08 +0200

    Commit zmian

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var myContainer = document.getElementById('mainForm');
+myContainer.innerHTML = "test";
+var App = /** @class */ (function () {
+    function App() {
+    }
+    return App;
+}());
---
+++ app.ts
@@ -0,0 +1,7 @@
+import {Form} from './form';
+
+let myContainer = document.getElementById('mainForm') as HTMLDivElement;
+myContainer.innerHTML = "test";
+class App {
+    activeForm : Form;
+}
