f38e09d9b21b01bb625292df9c163ea59f39ca70
Paweł Wójsik <pablo8899@interia.pl>
2020-06-14 08:11:06 +0200

    Usuniecie starego projektu na rzecz nowego z webpackiem

--- app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var myContainer = document.getElementById('mainForm');
-myContainer.innerHTML = "test";
-var App = /** @class */ (function () {
-    function App() {
-    }
-    return App;
-}());
--- app.ts
+++
@@ -1,7 +0,0 @@
-import {Form} from './form';
-
-let myContainer = document.getElementById('mainForm') as HTMLDivElement;
-myContainer.innerHTML = "test";
-class App {
-    activeForm : Form;
-}
